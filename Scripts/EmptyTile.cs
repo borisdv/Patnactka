9b3d77ee3f14ca479533347da84b6a23833072c8
borisdv <bdvarga@gmail.com>
2017-01-25 09:15:15 +0100

    Added Scripts

---
+++ Scripts/EmptyTile.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyTile : MonoBehaviour,IPuzzleTile {
+    //Begining position of raycasts
+    public Vector3 _raycastOrigin;
+
+    // X,Z coordinates in PuzzleField 
+    [SerializeField]
+    private int _x, _z;
+    // Number of a tile - set to 0
+    private int number=0;
+
+
+    /// <summary>
+    /// sets the origin of raycast , fires rays in 4 directions, and if a puzzlepiece is hit, sets the oposit freeMovement direction to the piece
+    /// </summary>
+    public void SetNeighborsFreeDirection()
+    {
+        _raycastOrigin = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        RaycastHit hit;
+        if (Physics.Raycast(_raycastOrigin,Vector3.left,out hit,0.8f))
+        {
+            hit.collider.gameObject.GetComponent<PuzzlePiece>().setFreeDirection(PuzzlePiece.Direction.RIGHT);
+        }
+        if (Physics.Raycast(_raycastOrigin, Vector3.right, out hit, 0.8f) )
+        {
+            hit.collider.gameObject.GetComponent<PuzzlePiece>().setFreeDirection(PuzzlePiece.Direction.LEFT);
+        }
+        if (Physics.Raycast(_raycastOrigin, Vector3.forward, out hit, 0.8f))
+        {
+            hit.collider.gameObject.GetComponent<PuzzlePiece>().setFreeDirection(PuzzlePiece.Direction.BACK);
+        }
+        if (Physics.Raycast(_raycastOrigin, Vector3.back, out hit, 0.8f) )
+        {
+            hit.collider.gameObject.GetComponent<PuzzlePiece>().setFreeDirection(PuzzlePiece.Direction.FORWARD);
+        }
+
+
+    }
+    /// <summary>
+    /// Returns the number/value of an piece
+    /// </summary>
+    /// <returns>Puzzle piece number</returns>
+    public int GetNumber()
+    {
+        return number;
+    }
+    /// <summary>
+    /// Sets the puzzle piece number
+    /// </summary>
+    /// <param name="number">Puzzle piece number</param>
+    public void SetNumber(int number)
+    {
+        this.number = number;
+    }
+    /// <summary>
+    /// Returns X coordinate in PuzzleField
+    /// </summary>
+    /// <returns>X coordinate</returns>
+    public int GetX()
+    {
+        return _x;
+    }
+    /// <summary>
+    /// Returns Z coordinate in PuzzleField
+    /// </summary>
+    /// <returns>Z coordinate</returns>
+    public int GetZ()
+    {
+        return _z;
+    }
+    /// <summary>
+    /// Sets X,Z coordinates in the PuzzleField
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="z"></param>
+    public void SetXZ(int x, int z)
+    {
+        _x = x;
+        _z = z;
+    }
+}
---
+++ Scripts/PuzzleGame.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleGame : MonoBehaviour
+{
+    public CanvasHandler canvas;
+    // Parent for Tiles
+    public Transform puzzlePool;
+    // Reference to puzzle field
+    private PuzzleField _puzzleField;
+    /// number of moves made by player to solve puzzle
+    [SerializeField]
+    private int _moves;
+    
+    /// <summary>
+    /// Called after a piece was moved, inceremnts the moves counter, resets all free movement directions to none, switches tiles
+    /// sets new free directions and checks if the game is solved
+    /// </summary>
+    /// <param name="pieceMoved">reference to a piece that was moved</param>
+    public void PieceMoved(PuzzlePiece pieceMoved)
+    {
+
+        _moves++;
+        canvas.SetMovesCount(_moves);
+        foreach (PuzzlePiece temp in puzzlePool.GetComponentsInChildren<PuzzlePiece>())
+        {
+            temp.setFreeDirection(PuzzlePiece.Direction.NONE);
+        }
+        _puzzleField.SwitchTiles(pieceMoved);
+
+        _puzzleField.SetMovableTiles();
+        if (_puzzleField.IsEmptyInRightCorner())
+        {
+            if (_puzzleField.IsSolved())
+            {
+                canvas.ShowSolvedMenu(_moves);
+            }
+        }
+
+
+
+    }
+
+    void Start()
+    {
+        _puzzleField = GetComponent<PuzzleField>();
+    }
+
+    public void NewGame()
+    {
+
+        canvas.SetMovesCount(0);
+        do
+        {
+            for (int i = 0; i < puzzlePool.childCount; i++)
+            {
+
+                Destroy(puzzlePool.GetChild(i).gameObject);
+            }
+            _puzzleField.InitField();
+           
+            Debug.Log(_puzzleField.IsSolvable());
+        }
+        while (!_puzzleField.IsSolvable());
+        }
+
+    
+    /// <summary>
+    /// Quits the game
+    /// </summary>
+#if UNITY_STANDALONE
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+#endif
+
+
+}
+
